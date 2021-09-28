a99a5cd22e1e72e1da6c10b45e4b3ea7ab4f8e87
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-28 16:30:08 +0300

    add module and controller

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
 /** @format */
 
-import { Controller, Module } from '@nestjs/common'
+import { Controller, Get, Module } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return 'hi there!'
+  }
+}
+
+@Module({
+  controllers: [AppController]
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  await app.listen(3000)
+}
+bootstrap()
